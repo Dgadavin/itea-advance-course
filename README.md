6bf2dda25d3aeedf0448d55a397b76ecdc871f14
Sotnikov Maksym <maxferiartos@gmail.com>
2020-08-07 14:25:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Devopsology base DevOps course
---
+++ jenkins/jobs/jenkinsfile_flow.groovy
@@ -0,0 +1,27 @@
+pipelineJob("test-pipeline") {
+    	logRotator {
+       		numToKeep(10)
+    	}
+
+    parameters {
+      choiceParam('ACTION', ["plan", "apply"], "Choose current action")
+      stringParam('COMMIT_HASH')
+	  booleanParam('BUILD_IMAGE', true, 'Build image')
+    }
+
+    definition {
+        cpsScm {
+   			scm {
+            	git {
+                  remote {
+                        github("dgadavin/devops-course-itea", 'https')
+                        branch("master")
+                  }
+              }
+        }
+        scriptPath("advance_course/jenkins/Jenkinsfile")
+      }
+
+    }
+
+ }
